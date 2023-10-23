4310c92d98b3f3d8b25a9043b1bef7c1921811e1
camsky10 <camsky10@gmail.com>
2023-10-23 13:53:30 -0400

    Starting outline

---
+++ ContinuousMonitor.java
@@ -0,0 +1,15 @@
+//SSD Project
+//CS4612 - 01
+//Cameron Cummings, Ethan Byrd, Matthew Fincher
+
+public class ContinuousMonitor {
+    public static void main(String[] args) {
+        while(true) {
+            update();
+        }
+    }    
+
+    public static void update() {
+
+    }
+}
