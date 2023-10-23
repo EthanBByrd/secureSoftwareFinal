7d28a2ab1555fe6f5db113c0fa40b0ed9e0133a4
camsky10 <camsky10@gmail.com>
2023-10-23 14:59:00 -0400

    methods to implement added

--- ContinuousMonitor.java
+++ ContinuousMonitor.java
@@ -2,8 +2,13 @@
 //CS4612 - 01
 //Cameron Cummings, Ethan Byrd, Matthew Fincher
 
+import java.util.*;
+import java.lang.*;
+
 public class ContinuousMonitor {
     public static void main(String[] args) {
+        //while loop to continuously update/monitor the software
+        
         while(true) {
             update();
         }
@@ -11,5 +16,47 @@ public class ContinuousMonitor {
 
     public static void update() {
 
+    }
+
+    //THE FOLLOWING CODES SHOULD RETURN 1 IF CLEAR, 0 IF NOT
+
+    //checks for basic buffer attacks
+    public static int checkBuffer() {
+
+    }
+
+    //checks for basic libc attacks
+    public static int checkLibc() {
+        
+    }
+
+    //check for string format attacks
+    public static int checkStringFormat() {
+        
+    }
+
+    //check for heap attacks 
+    public static int checkHeap() {
+        
+    }
+
+    //check for shellshock attacks
+    public static int checkShellshock() {
+        
+    }
+
+    //check for race condition vulnerabilities
+    public static int checkRaceCondition() {
+        
+    }
+
+    //check for basic web attacks
+    public static int checkWebAttack() {
+        
+    }
+
+    //check for SQL injection attacks
+    public static int checkSQL() {
+        
     }
 }
